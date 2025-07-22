f7c8a111e1a535c860b5b3b32829261f1111aba1
JSON XHR <126494515+Divjason@users.noreply.github.com>
2025-07-22 23:03:49 +0900

    Add files via upload

---
+++ 250721/250721_Proj02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+#define PI 3.141592f
+
+int main()
+{
+	printf("PI is %f\n", PI);
+	printf("Biggest int: %d\n", INT_MAX);
+	printf("One Byte in this system is %d bits\n", CHAR_BIT);
+	printf("Smallest normal float %e\n", FLT_MIN);
+
+	return 0;
+}
